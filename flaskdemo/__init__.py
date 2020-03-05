db2312e0e383b8c50fa1e8aebff45009bf48abcc
Joe Clark <clark_joe@bah.com>
2020-03-04 22:21:34 -0500

    Added upload page, a page that tests out taking variables in the route path, and a bunch of things I'm messing around with

--- flaskdemo/__init__.py
+++ flaskdemo/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 
+UPLOAD_FOLDER = '/sites/flaskdemo'
+
 app = Flask(__name__,
             template_folder='templates',
             static_folder='static')
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from flaskdemo import routes
--- flaskdemo/routes.py
+++ flaskdemo/routes.py
@@ -1,7 +1,68 @@
-from flask import render_template
+import os
+
+from flask import render_template, flash, request, redirect
 from flaskdemo import app
+from werkzeug.utils import secure_filename
 
 agency = 'U.S. Department of Labor'
+
+
+ALLOWED_EXTENSIONS = {'pdf'}
+DOL = 'U.S. Department of Labor'
+AGENCIES = {
+    'whd': 'Wage and Hour Division',
+    'brb': 'Benefits Review Board'
+}
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def clean(string):
+    string = string.replace('\n', ' ')
+    return string
+
+
+def pdf_info(filename):
+    import textract
+    text = textract.process(filename)
+    return text.decode('utf-8')
+
+
+@app.route('/upload')
+def upload_form():
+    return render_template('upload.html',
+                           title=agency)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(filepath)
+            flash('File successfully uploaded')
+            info = pdf_info(filepath)
+            return render_template('upload.html',
+                                   title=agency,
+                                   info=info)
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            error = 'Error status'
+            # return redirect(request.url)
+            # return redirect(url_for('upload_form', error=error))
+            return render_template('upload.html',
+                                   title=agency,
+                                   error=error)
 
 
 @app.route('/')
@@ -9,6 +70,17 @@ def home():
     """Landing page."""
     return render_template('home.html',
                            title=agency)
+
+
+@app.route('/demo/<path:variable>', methods=['GET'])
+def demo_variable(variable):
+    """Demo variable page."""
+    ag = AGENCIES.get(variable)
+    if not ag:
+        ag = DOL
+    return render_template('demo.html',
+                           title=ag,
+                           h1='Hello World')
 
 
 @app.route('/demo')
